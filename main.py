00a303d80e96fa63519fcc5b4e99a1525ef1403d
Borzigenti <1heted@hdsb.ca>
2021-03-29 18:40:39 -0400

    zsryjs5

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ rightcolor = ColorSensor(Port.S1)
 leftcolor = ColorSensor(Port.S2)
 gyroSensor = GyroSensor(Port.S3)
 accangle = 0 # accumulated angle, angle you're supposed to be at. 
+log = False
 
 black = 3
 white = 41
@@ -55,7 +56,7 @@ def linesquare(speed=150):
     if rorl == "r":
         while leftcolor.reflection() > 4:
             pass
-        rightmotor.hold()jw
+        rightmotor.hold()
         leftmotor.hold()
         for i in range(2):
             if i % 2 == 0:
@@ -86,46 +87,32 @@ def linesquare(speed=150):
                 rightmotor.hold()
 
 def line_follow(colour_sensor, speed, P_multiplier, D_multiplier, I_multiplier, sample_time, distance):
+    global log
     robot.reset()
     last_error = 0
-    e_count = 0
+    iteration = 0
     p = 0
     d = 0
     i = 0
     error = 0
+    total = (p + (i*I_multiplier) + d)
+    if log == True:
+        data = DataLog('total', 'p', 'i', 'd', 'error', 'last error')
     while robot.distance() < distance:
         error = threshold - colour_sensor.reflection()
         p = error * P_multiplier
         d = (error - last_error) / sample_time * D_multiplier
-        i = error + i
-        #print('p:', p, 'd:', d, 'error:', error, 'ecount:', e_count, 'speed:', speed)
+        i = error * sample_time + i
+        total = (p + (i*I_multiplier) + d)
+        if log == True:
+            data.log(total, p, i*I_multiplier, d, error, last_error)
         #this stays on the right side of the line
-        robot.drive(speed, (p + d + (i*I_multiplier)))
+        robot.drive(speed, total)
         last_error = error
         time.sleep(sample_time)
     robot.stop()
     leftmotor.hold()
     rightmotor.hold()
-    #robot.reset()
-    #last_error = 0
-    #e_count = 0
-    #while robot.distance() < distance:
-    #    p = 0
-    #    d = 0
-    #    error = 0
-
-    #    error = threshold - colour_sensor.reflection()
-    #    p = error * P_multiplier
-    #    d = (error - last_error) / sample_time * D_multiplier
-    #    #print('p:', p, 'd:', d, 'error:', error, 'ecount:', e_count, 'speed:', speed)
-    #    #this stays on the right side of the line
-    #    robot.drive(speed, (p + d))
-    #    last_error = error
-    #    time.sleep(sample_time)
-    #robot.stop()
-    #leftmotor.hold()
-    #rightmotor.hold()
-
 
 #positive number turns counterclockwise
 def turntoangle(Target_angle, gyroturn=False, sped=150, turntimes=1):
@@ -218,11 +205,11 @@ while True:
         #Run 1
         accangle = 0
         gyroSensor.reset_angle(0)
-        straight(200, 200)
-        line_follow(rightcolor, 200, 1.8, 0.08, 0.1, 0.01, 200)
-        straight(100, 200)
-        turntoangle(60, True)
-        line_follow(rightcolor, 200, 1.5, 0.08, 0.1, 0.01, 330)
+        straight(200, 200) # moving to follow line that leads to line that leads to share model
+        line_follow(rightcolor, 200, 1.8, 0.08, 0.1, 0.01, 200) #  follow line that leads to line that leads to share model
+        straight(100, 200) # drive forwards a bit
+        turntoangle(60, True) # turn to follow line that leads to share model
+        line_follow(rightcolor, 200, 1.5, 0.08, 0.1, 0.01, 330) 
         straight(50, 130, sampleFactor=3) # drops red cube
         straight(30, -200)
         turntoangle(31.5, True)
@@ -290,54 +277,70 @@ while True:
         #health units, minifigure on tire, weight machine, drop bricks
     elif Button.LEFT in ev3.buttons.pressed():
         
-        #ev3.speaker.say('Run 3')
         # set point 1 for gyro (parallel to south wall)
         accangle = 0
         gyroSensor.reset_angle(0)
-        straight(250, 200)
-        line_follow(rightcolor, 200, 1.6, 0.1, 0.1, 0.01, 420)
+
         #health units
-        turntoangle(19)
-        #straight(50, 100)
-        mediummotor.run_angle(500, 6.5 * 90)#health unit
-        straight(50, -100)
-        mediummotor.run_angle(500, 5.5 * 90)
-        straight(50, -100)
-        turntoangle(-2.5, True)
-        accangle = 0
-        mediummotor.run_angle(400, -5 * 90, wait=False)
-        line_follow(rightcolor, 250, 1.5, 0.1, 0.1, 0.01, 950)
+        straight(250, 200) #move to first line follow
+        line_follow(rightcolor, 200, 1.6, 0.02, 0.1, 0.01, 420) #linefollow to health unit spot
+        turntoangle(21)#adjust to health unit
+        mediummotor.run_angle(500, 6.5 * 90)# drop health units
+        straight(50, -100)# moving to get health unit out of claw
+        mediummotor.run_angle(500, 5.5 * 90) # getting health unit out of claw
+        straight(50, -100) # move back to line
+
+        #Minifigure
+        mediummotor.run_angle(400, -5 * 90, wait=False) # moving arm up for minifig
+        wait(300)
+        line_follow(rightcolor, 100, 1.5, 0.02, 0.1, 0.01, 150) # making sure robot stays on line
+        log = True
+        line_follow(rightcolor, 200, 1.5, 0.02, 0.1, 0.01, 820) # linefollow to treadmill
+        log = False
         # turn to angle
-        turntoangle(90)
+        turntoangle(90) #turning so bot is perpendicular to line
         #gyro_turn(90)
-        straight(170, 300, 3)
+        straight(190, 300, 3) #moving to big tire
         #mediummotor.run_angle(400, -5 * 90)
-        turntoangle(49, True)
+        turntoangle(139) #turning to drop minifigure
         wait(500)
-        #mediummotor.run_angle(500, 8 * 90) # minifigure drop
-        mediummotor.run_angle(250, 8 * 90)
-        mediummotor.run_angle(500, -1180, wait=False)
+        mediummotor.run_angle(250, 9 * 90) # dropping minifigure
+
+        #weight machine
+        mediummotor.run_angle(500, -10 * 90, wait=False)#preparing for weight machine
         turntoangle(90, sped=100)#turns to linesquare
-        turntoangle(90, sped=100)
+        turntoangle(90, sped=100)#again, just to make sure
         wait(700)
-        straight(130, 200)
-        linesquare()
-        linesquare()
+        straight(120, 200)#moving close to line
+        linesquare()#linesquaring
+        linesquare()#again, just to make sure
         wait(500)
-        turntoangle(-17, True) # turns from linesquare to line up with weight machine
+        turntoangle(78) # turns from linesquare to line up with weight machine
         wait(500)
-        oldstraight(140, 150)
+        oldstraight(140, 150) #move to weight machine
+        while Button.CENTER not in ev3.buttons.pressed():
+            pass
+        robot.turn(5) # turning so arm touches weight machine
+        while Button.CENTER not in ev3.buttons.pressed():
+            pass
         mediummotor.run_angle(500, 17 * 90) #weight machine push down
-        mediummotor.run_angle(-500, 15 * 90, wait=False)
-        turntoangle(180, sped=100)
-        line_follow(rightcolor, 200, 1.8, 0.12, 0.1, 0.01, 320)
-        straight(580, 200, 3)
-        turntoangle(45, True)
+        mediummotor.run_angle(-500, 15 * 90, wait=False)# weight machine release
+
+        #drop boccia bricks
+        turntoangle(180, sped=100) # turn to follow line beside weight machine
+        log = True
+        #line_follow(leftcolor, 200, 1.3, 0.03, 1, 0.01, 100)
+        line_follow(leftcolor, 200, 1.3, 0.03, 1, 0.01, 320) #follow line beside weight machine 
+        log = False
+        straight(580, 200, 3) # go a bit further, the line squigles
+        turntoangle(225) #turn so back is facing target
         straight(290, -300) # backing in to release bricks
         mediummotor2.run_angle(1000, -355)#releasing bricks
         wait(500)
-        mediummotor2.run_angle(800, 370, wait=False)
-        straight(1200, 500)
+
+        #back to home
+        mediummotor2.run_angle(800, 370, wait=False) # bringing box bottom back up, for testing purposes
+        straight(1200, 500) # back to home
 
 
         #step counter, through pull-up bar, dance
